f7c84ce9875a3c6655ed51e6b6828deede342911
Allison <farrella95@gmail.com>
2022-09-29 16:53:30 -0400

    stuff

--- 02-Challenge/Assets/app.js
+++ 02-Challenge/Assets/app.js
@@ -1,13 +1,17 @@
-
+var appid = "3de3f45fef91dfdfd3378f39fe973f00";
+var city = "Savannah";
 
 function getFetch() {
-  
-  const url = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=3de3f45fef91dfdfd3378f39fe973f00`;
+  const url =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    appid;
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-    }); 
+    });
 }
 
-getFetch()
+getFetch();
