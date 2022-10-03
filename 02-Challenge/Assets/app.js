08ad7ea3919f7cb8de92be3fcb99d688f4dafd8a
Allison <farrella95@gmail.com>
2022-10-02 20:36:15 -0400

    updated

--- 02-Challenge/Assets/app.js
+++ 02-Challenge/Assets/app.js
@@ -1,17 +1,21 @@
-var appid = "3de3f45fef91dfdfd3378f39fe973f00";
-var city = "san diego";
+var apiKey = "3de3f45fef91dfdfd3378f39fe973f00";
+var city = "atlanta";
 
 function getFetch() {
   const url =
     "http://api.openweathermap.org/data/2.5/weather?q=" +
     city +
     "&appid=" +
-    appid;
+    apiKey;
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      document.querySelector(".mainSection").innerText = data.main.temp;
+      document.querySelector(".temperature").innerHTML =
+        "Temp:" + data.main.temp;
+      document.querySelector(".wind").innerHTML = "Wind:" + data.wind.speed;
+      document.querySelector(".humid").innerHTML =
+        "Humidity:" + data.main.humidity;
     });
 }
 
