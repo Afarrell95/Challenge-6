8ac1535afbe43af631b0620b581df2265349de5f
Allison <farrella95@gmail.com>
2022-10-05 18:24:41 -0400

    update

--- 02-Challenge/Assets/app.js
+++ 02-Challenge/Assets/app.js
@@ -1,4 +1,3 @@
-// var apiKey = "3de3f45fef91dfdfd3378f39fe973f00";
 var searchBtn = document.querySelector("searchBttn");
 var city = "san diego";
 
@@ -10,7 +9,7 @@ function getFetch() {
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      // var city = document.querySelector("input").value;
+      //current day
       document.querySelector(".temperature").innerHTML =
         "Temp:" + data.current.temp;
       document.querySelector(".wind").innerHTML =
@@ -18,35 +17,35 @@ function getFetch() {
       document.querySelector(".humid").innerHTML =
         "Humidity:" + data.current.humidity;
       document.querySelector(".uvi").innerHTML = "UV Index:" + data.current.uvi;
-
+      //fdf day one
       document.querySelector(".day-one-temp").innerHTML =
         data.daily[0].temp.day;
       document.querySelector(".day-one-wind").innerHTML =
         data.daily[0].wind_speed;
       document.querySelector(".day-one-humid").innerHTML =
         data.daily[0].humidity;
-
+      //fdf day two
       document.querySelector(".day-two-temp").innerHTML =
         data.daily[1].temp.day;
       document.querySelector(".day-two-wind").innerHTML =
         data.daily[1].wind_speed;
       document.querySelector(".day-two-humid").innerHTML =
         data.daily[1].humidity;
-
+      //fdf day three
       document.querySelector(".day-three-temp").innerHTML =
         data.daily[2].temp.day;
       document.querySelector(".day-three-wind").innerHTML =
         data.daily[2].wind_speed;
       document.querySelector(".day-three-humid").innerHTML =
         data.daily[2].humidity;
-
+      //fdf day four
       document.querySelector(".day-four-temp").innerHTML =
         data.daily[3].temp.day;
       document.querySelector(".day-four-wind").innerHTML =
         data.daily[3].wind_speed;
       document.querySelector(".day-four-humid").innerHTML =
         data.daily[3].humidity;
-
+      //fdf day five
       document.querySelector(".day-five-temp").innerHTML =
         data.daily[4].temp.day;
       document.querySelector(".day-five-wind").innerHTML =
@@ -56,4 +55,9 @@ function getFetch() {
     });
 }
 
-getFetch();
+function setToLocal() {
+  let history = document.querySelector("input");
+  console.log(history);
+}
+
+setToLocal();
