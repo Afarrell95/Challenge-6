3e60935e56407e53230b5a6db83e673b4fb39941
Allison <farrella95@gmail.com>
2022-10-05 18:08:17 -0400

    api info to screen

--- 02-Challenge/Assets/app.js
+++ 02-Challenge/Assets/app.js
@@ -17,6 +17,42 @@ function getFetch() {
         "Wind:" + data.current.wind_speed;
       document.querySelector(".humid").innerHTML =
         "Humidity:" + data.current.humidity;
+      document.querySelector(".uvi").innerHTML = "UV Index:" + data.current.uvi;
+
+      document.querySelector(".day-one-temp").innerHTML =
+        data.daily[0].temp.day;
+      document.querySelector(".day-one-wind").innerHTML =
+        data.daily[0].wind_speed;
+      document.querySelector(".day-one-humid").innerHTML =
+        data.daily[0].humidity;
+
+      document.querySelector(".day-two-temp").innerHTML =
+        data.daily[1].temp.day;
+      document.querySelector(".day-two-wind").innerHTML =
+        data.daily[1].wind_speed;
+      document.querySelector(".day-two-humid").innerHTML =
+        data.daily[1].humidity;
+
+      document.querySelector(".day-three-temp").innerHTML =
+        data.daily[2].temp.day;
+      document.querySelector(".day-three-wind").innerHTML =
+        data.daily[2].wind_speed;
+      document.querySelector(".day-three-humid").innerHTML =
+        data.daily[2].humidity;
+
+      document.querySelector(".day-four-temp").innerHTML =
+        data.daily[3].temp.day;
+      document.querySelector(".day-four-wind").innerHTML =
+        data.daily[3].wind_speed;
+      document.querySelector(".day-four-humid").innerHTML =
+        data.daily[3].humidity;
+
+      document.querySelector(".day-five-temp").innerHTML =
+        data.daily[4].temp.day;
+      document.querySelector(".day-five-wind").innerHTML =
+        data.daily[4].wind_speed;
+      document.querySelector(".day-five-humid").innerHTML =
+        data.daily[4].humidity;
     });
 }
 
